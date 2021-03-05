724681569f08fbc022e5ba5ff8fcb4a35e4d1ff0
WeiLi <lw@blask.cn>
2021-03-05 17:01:38 +0800

    算法改进

--- 1768Strings/src/Test.java
+++ 1768Strings/src/Test.java
@@ -12,4 +12,22 @@ class Solution {
         }
         return result;
     }
+
+    /**
+     * 该解法节省时间和内存
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public String mergeAlternatelyNewSolution(String word1, String word2) {
+        int len1 = word1.length(), len2 = word2.length(), idx = 0;
+        char[] res = new char[len1 + len2];
+        for(int i = 0; i < len1 || i < len2; ++i) {
+            if(i < len1) res[idx++] = word1.charAt(i);
+            if(i < len2) res[idx++] = word2.charAt(i);
+        }
+        return new String(res);
+    }
 }
+
+
